81837c16959ada361d7698d05e7b90ad14a8a47c
Nicholas Pereira <nicholasfelliperp@gmail.com>
2020-07-11 20:56:28 -0300

    Home
    Projeto Finalizado

---
+++ js/main.js
@@ -0,0 +1,118 @@
+// function clicado(){
+//     alert("Obrigado por clicar!!");    
+// }
+
+//   function clicou(){
+//      document.getElementById("agradecimento").innerHTML = "Obrigado Por Clicar!";
+//  }
+//// REDIRECIONAR PARA OUTRA PAGINA
+
+// function redirecionar(){
+//    // window.open("https://pt-br.facebook.com/globallabsbr/");//*NOVA GUIA
+//     window.location.href = "https://pt-br.facebook.com/globallabsbr/";//MESMA GUIA
+
+// }
+
+
+// function trocar(elemento){
+//   //  document.getElementById("mousemove").innerHTML = "Obrigado Por passar o mouse!";
+//     elemento.innerHTML= "Obrigado Por passar o mouse!";
+   
+// }
+// function voltar(elemento){
+//    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui!";
+//     elemento.innerHTML="Passe o mouse aqui!";
+// }
+
+
+// function load(){ //Quando carregar pagina ele carrega a funcao
+//     alert("pagina carregada");
+// }
+
+// function funcaoChange(elemento){   //Pega o valor digitado pelo Usuario
+//  console.log(elemento.value);
+// }
+//var nome="Nicholas"
+ //var idade = 24;
+//var frase="Japão é o melhor time do mundo!";
+// alert(nome+ " tem "+idade+" anos");
+//var lista = ["banana","maca","laranja","pera","uva"]; // ARRAY
+// console.log(nome);
+// console.log(idade);
+// console.log(frase);
+// console.log(frase.replace("Japão","Brasil"));
+
+
+//lista.push("uva"); //INSERE
+//lista.pop(); //REMOVE
+//console.log(lista.length); //TAMANHO DO ARRAY
+//console.log(lista.toString()); //CONVERTE PARA STRING
+//console.log(lista.join(" - ")); //CONVERTE PARA STRING E POSSIBILITA ADICIONAR ALGO ENTRE CADA ELEMENTO
+
+
+///var fruta =[{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}] ; //DICIONARIO
+//console.log(fruta.nome); 
+//console.log(fruta);
+
+
+//VALIDAR :: IF  ELSE :: WHILE
+//var idade = prompt("Qual sua idade?"); //PROMPT É UM QUESTIONARIO
+//ABAIXO SERVE PARA VALIDAR O SITE COM RELACAO DA IDADE, SITE PARA MAIORES
+// if(idade>=18){
+//     alert("maior de idade!");
+// }else{
+//     alert("menor de idade!");
+// }
+
+
+// //WHILE
+ // var count = 0;
+// while(count<5){
+//     console.log(count);
+//     count++;
+
+// }
+
+// //FOR
+// var count;
+// for(count=1;count<=5;count++){
+//     console.log(count);
+// }
+
+//var d = new Date();
+//alert(d.getMonth()+1); //MES
+// alert(d.getDay()); //DIA
+// alert(d.getHours()); //HORA
+// alert(d.getMinutes()); //MINUTO
+
+
+//--------------FUNCOES----------------------------
+// function soma(n1,n2) {
+//     return n1+n2;
+// }
+// alert(soma(5,10));
+
+// function setReplace(frase,nome,novo_nome){
+//     return frase.replace(nome,novo_nome);
+// }
+// alert(setReplace("Vai Japao", "Japao","Brasil"));
+
+
+
+// var validar = 0;// VARIAVEL GLOBAL
+// function validaIdade(idade){
+//     var validar = true; // VARIAVEL LOCAL
+//     if(idade>=18){
+//         validar = true;
+//     }else{
+//         validar = false;
+//     }
+// return validar;
+// }
+
+// var idade = prompt("Qual sua idade?");
+// console.log(validaIdade(idade));
+
+
+
+
